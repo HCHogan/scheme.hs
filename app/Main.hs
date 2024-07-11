f7bed30347e6dfdae8c161aa042874ab5dfd08fb
hank <ysh2291939848@outlook.com>
2024-07-11 23:18:09 +0800

    lispval

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import System.Environment
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  args <- getArgs
+  putStrLn $ "Hello, " ++ show args
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,48 @@
-module MyLib (someFunc) where
+module MyLib (someFunc, readExpr) where
+
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+  _ <- char '"'
+  x <- many (noneOf "\"")
+  _ <- char '"'
+  return $ String x
+
+-- An `atom` is a letter or symbol, followed by any number of letters, digits, or symbols
+parseAtom :: Parser LispVal
+parseAtom = do 
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
